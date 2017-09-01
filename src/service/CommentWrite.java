f1ebac762c80672d3ae7ab00971f653b776e8b1b
soonjae <jujak2874@gmail.com>
2017-09-01 11:58:03 +0900

    mod

---
+++ src/service/CommentWrite.java
@@ -0,0 +1,12 @@
+package service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CommentWrite implements CommandProcess{
+	@Override
+	public String requestPro(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
